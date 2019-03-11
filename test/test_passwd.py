e1c0f983e8d1d67169c647eb57c9e373a1d22b09
Tyler Roberts <tjroberts314@gmail.com>
2019-03-11 13:31:21 -0400

    write more tests for user

--- test/test_passwd.py
+++ test/test_passwd.py
@@ -7,7 +7,7 @@ class TestPasswd(unittest.TestCase):
     def setUp(self):
         self.get = passwd.Get("passwd", "group")
 
-    def _strings_to_list_of_dicts(self, *args):
+    def _to_list(self, *args):
         output = []
         for arg in args:
             name, password, uid, gid, comment, home, shell = arg.strip().split(":")
@@ -41,12 +41,38 @@ class TestPasswd(unittest.TestCase):
                 self.assertEqual(shell, users[name]["shell"])
 
     def test_single_query(self):
-        self.assertEqual(self.get.users(name="daemon"), self._strings_to_list_of_dicts("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
-        self.assertEqual(self.get.users(uid="3"), self._strings_to_list_of_dicts("adm:x:3:4:adm:/var/adm:/sbin/nologin"))
+        self.assertEqual(self.get.users(name="daemon"), self._to_list("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
+        self.assertEqual(self.get.users(uid="3"), self._to_list("adm:x:3:4:adm:/var/adm:/sbin/nologin"))
+        self.assertEqual(self.get.users(uid="38"), self._to_list("ntp:x:38:38::/etc/ntp:/sbin/nologin"))
+        self.assertEqual(self.get.users(uid="14"), self._to_list("ftp:x:14:50:FTP User:/var/ftp:/sbin/nologin"))
+        self.assertEqual(self.get.users(gid="50"), self._to_list("ftp:x:14:50:FTP User:/var/ftp:/sbin/nologin"))
+        self.assertEqual(self.get.users(comment="virtual console memory owner"), self._to_list("vcsa:x:69:69:virtual console memory owner:/dev:/sbin/nologin"))
+        self.assertEqual(self.get.users(home="/var/spool/mqueue"), self._to_list("mailnull:x:47:47::/var/spool/mqueue:/sbin/nologin", "smmsp:x:51:51::/var/spool/mqueue:/sbin/nologin"))
         compare = []
         with open("passwd", "r") as f:
             for line in f:
                 name, password, uid, gid, comment, home, shell = line.strip().split(":")
                 if shell == "/sbin/nologin":
                     compare.append(line.strip())
-        self.assertEqual(self.get.users(shell="/sbin/nologin"), self._strings_to_list_of_dicts(*compare))
+        self.assertEqual(self.get.users(shell="/sbin/nologin"), self._to_list(*compare))
+
+    def test_bad_query(self):
+        self.assertEqual(self.get.users(name="foo"), [])
+        self.assertEqual(self.get.users(uid="-1"), [])
+        self.assertEqual(self.get.users(gid="bar"), [])
+        self.assertEqual(self.get.users(comment="bad comment"), [])
+        self.assertEqual(self.get.users(home="/not/real"), [])
+        self.assertEqual(self.get.users(shell="false"), [])
+        self.assertEqual(self.get.users(name="daemon", uid="2", gid="2", shell="/etc/news"), [])
+
+    def test_multiple_queries(self):
+        self.assertEqual(self.get.users(name="daemon", uid="2"), self._to_list("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
+        self.assertEqual(self.get.users(name="daemon", uid="2", gid="2"),
+                         self._to_list("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
+        self.assertEqual(self.get.users(name="daemon", uid="2", comment="daemon"),
+                         self._to_list("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
+        self.assertEqual(self.get.users(name="daemon", uid="2", comment="daemon", home="/sbin", shell="/sbin/nologin"),
+                         self._to_list("daemon:x:2:2:daemon:/sbin:/sbin/nologin"))
+        self.assertEqual(self.get.users(name="mailnull", home="/var/spool/mqueue"),
+                         self._to_list("mailnull:x:47:47::/var/spool/mqueue:/sbin/nologin"))
+        self.assertEqual(self.get.users(uid="86", gid="86", shell="/sbin/nologin"), self._to_list("sabayon:x:86:86:Sabayon user:/home/sabayon:/sbin/nologin"))
